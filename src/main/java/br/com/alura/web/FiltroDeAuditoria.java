97f9b3417e81a50cfb055023e0b62ae77580562a
Sidarta <sidarta_rocha@yahoo.com.br>
2017-11-27 18:46:35 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/web/FiltroDeAuditoria.java
@@ -0,0 +1,55 @@
+package br.com.alura.web;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import br.com.alura.gerenciador.Usuario;
+
+@WebFilter(urlPatterns="/*")
+public class FiltroDeAuditoria implements Filter{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+			HttpServletRequest req = (HttpServletRequest) request;
+			
+			String usuario = getUsuario(req);
+		
+			String uri = req.getRequestURI();
+			System.out.println("Usuario:" + usuario +"acessando URI:" + uri);
+			chain.doFilter(request, response);
+		
+	}
+
+	private String getUsuario(HttpServletRequest req) {
+		Usuario usuario = (Usuario) req.getSession().getAttribute("usuarioLogado");
+		
+		if(usuario!=null) {
+			return usuario.getEmail();
+		}else {
+			return "<deslogado>";
+		}
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
